ae49c4150cc592b8d7d17985b22d87bb752d0ca7
Jacques <dsicrod@gmail.com>
2020-07-16 15:57:34 +0200

    Documented EthyfierDB::String

--- EthyfierDB/string.h
+++ EthyfierDB/string.h
@@ -4,16 +4,36 @@
 #include "pch.h"
 
 namespace EthyfierDB {
+	/// <summary>
+	/// Represents an EthyfierDB's String item.
+	/// </summary>
 	class String : public Item
 	{
 	public:
+		/// <summary>
+		/// Create a new String.
+		/// </summary>
+		/// <param name="name">Name of the String.</param>
+		/// <param name="value">Content of the String.</param>
 		String(const std::wstring& name,
 			const std::wstring& value);
 
+		/// <summary>
+		/// Returns the type of the Item.
+		/// </summary>
+		/// <returns>EthyfierDB::ItemType::String</returns>
 		ItemType getType() override;
 
+		/// <summary>
+		/// Get the value of the String.
+		/// </summary>
+		/// <returns>Value of the String.</returns>
 		const std::wstring getValue();
 
+		/// <summary>
+		/// Change the value of the String.
+		/// </summary>
+		/// <param name="newValue">New String value.</param>
 		void setValue(const std::wstring& newValue);
 
 	private:
