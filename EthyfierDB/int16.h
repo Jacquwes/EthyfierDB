1c79e2b8f7b468e9dea7fc5e3c69023714ced42e
Jacques <dsicrod@gmail.com>
2020-07-16 23:17:22 +0200

    Documented Int16

--- EthyfierDB/int16.h
+++ EthyfierDB/int16.h
@@ -4,16 +4,36 @@
 #include "pch.h"
 
 namespace EthyfierDB {
+	/// <summary>
+	/// Used to contains 16 bits wide numbers.
+	/// </summary>
 	class Int16 : public Item
 	{
 	public:
+		/// <summary>
+		/// Create an EthyfierDB's Int16 item.
+		/// </summary>
+		/// <param name="name">Name of the item.</param>
+		/// <param name="value">Value of the item.</param>
 		Int16(const std::wstring& name,
 			const int16_t& value);
 
+		/// <summary>
+		/// Returns the type of the Item.
+		/// </summary>
+		/// <returns>EthyfierDB::ItemType::Int16</returns>
 		ItemType getType() override;
 
+		/// <summary>
+		/// Return the value of the Item.
+		/// </summary>
+		/// <returns>Value of the Item.</returns>
 		int16_t getValue();
 
+		/// <summary>
+		/// Change the value of the Item.
+		/// </summary>
+		/// <param name="newValue">New value of the Item.</param>
 		void setValue(const int16_t& newValue);
 
 	private:
